505313821b627ebbafd80e0a637b2e9ed4a44995
tawfiqealahi <tawfiqealahi99@gmail.com>
2023-10-20 17:47:37 +0600

    dark light mode full

--- tawfiq alahi/main.js
+++ tawfiq alahi/main.js
@@ -3,6 +3,12 @@
 function myFunction() {
   var element = document.body;
   element.classList.toggle("dark-mode");
+  var x = document.getElementById("myDIV");
+  if (x.innerHTML === "Night") {
+    x.innerHTML = "Light";
+  } else {
+    x.innerHTML = "Night";
+  }
 }
 
 /* ========== dark mode end =========== */
